a8c3603095ce7b7d3ac93c875a0c3c41d05e83f9
hadeer14 <hadeersayedk@gmail.com>
2026-07-04 23:07:27 +0300

    fix firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0tYmS6m10kjRRhxjd19bbwaeYGVNBaFg",
+  apiKey: "AIzaSyAOtYmS6m10kjRRhxjd19bbwaeYGVNBaFg",
   authDomain: "webqa-alathar.firebaseapp.com",
   projectId: "webqa-alathar",
   storageBucket: "webqa-alathar.firebasestorage.app",
   messagingSenderId: "456435070024",
   appId: "1:456435070024:web:cade68383a9c981a6c947d",
+  measurementId: "G-MC9KYZCL3F"
 };
 
 const app = initializeApp(firebaseConfig);
